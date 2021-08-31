a030b4b3424f803b0eab33dd3a38c2af81ec8de6
Christopher Wagner <cwagne17@students.towson.edu>
2021-08-31 10:56:28 -0400

    changed route

--- src/modules/user-service/UserServiceController.ts
+++ src/modules/user-service/UserServiceController.ts
@@ -36,25 +36,6 @@ export class UserServiceController implements Controller {
 
         /**
          * @swagger
-         * /users/{userId}:
-         *  get:
-         *      description: Gets a single user via user id
-         *      tags:
-         *          - User Service
-         *      responses:
-         *          200:
-         *              description: Gets a single user
-         *              content:
-         *                  application/json:
-         *                      schema:
-         *                          $ref: '#/components/schemas/User'
-         *          404:
-         *              description: User does not exist
-         */
-        router.route('/users/:userId').get(this.proxyRequest((req: Request) => `/users/${encodeURIComponent(req.params.userId)}`));
-
-        /**
-         * @swagger
          * /guidelines/members/{memberId}:
          *  put:
          *      description: Adds a new mapper
@@ -886,11 +867,17 @@ export class UserServiceController implements Controller {
          *          - User Service
          *      parameters:
          *          - in: path
-         *            name: username
+         *            name: user
          *            schema:
          *                type: string
-         *            required: false
-         *            description: The username of the user
+         *            required: true
+         *            description: The identifier of the user (userId or username)
+         *          - in: query
+         *            name: q
+         *            schema:
+         *              type: string
+         *            required: true
+         *            description: Identifies whether querying by userId or username
          *      responses:
          *          200:
          *              description: OK
@@ -908,7 +895,7 @@ export class UserServiceController implements Controller {
          *          404:
          *              description: NOT FOUND - Unable to find user
          */
-        router.get('/users/:username', this.proxyRequest((req: Request) => USER_ROUTES.FETCH_USER(req.params.username)));
+        router.get('/users/:user', this.proxyRequest((req: Request) => `/users/${encodeURIComponent(req.params.user)}?${querystring.stringify(req.query)}`));
 
         /**
          * @swagger
