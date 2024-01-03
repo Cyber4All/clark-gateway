ab2c3d4e549194b3594298b69059678280b1419d
Christohper Wagner <cwagne17@students.towson.edu>
2024-01-03 14:59:50 -0500

    add metrics imports

--- src/modules/clark/metrics/metrics.router.ts
+++ src/modules/clark/metrics/metrics.router.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { buildProxyRouter } from "../../../shared/functions/build-proxy-router";
+import { METRICS_ROUTES } from "./metrics.routes";
 
 export class MetricsRouteHandler {
     public static build(): Router {
