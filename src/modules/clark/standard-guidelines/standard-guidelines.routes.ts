a1eaeec2a4022834c1f57175993354262002b04d
Christohper Wagner <cwagne17@students.towson.edu>
2024-01-03 17:08:53 -0500

    add standard guideliens routes

--- src/modules/clark/standard-guidelines/standard-guidelines.routes.ts
+++ src/modules/clark/standard-guidelines/standard-guidelines.routes.ts
@@ -1,3 +1,93 @@
+import { HTTPMethod } from "../../../shared/types/http-method.type";
 import { ProxyRoute } from "../../../shared/types/proxy-route.type";
 
-export const STANDARD_GUIDELINES_ROUTES: ProxyRoute[] = [];
+export const STANDARD_GUIDELINES_ROUTES: ProxyRoute[] = [
+    /**
+     * The following routes have not been declared by
+     * clark-service yet and will have a different target
+     * than clark-service
+     *
+     * The target for the following routes shoud be standard guidelines
+     */
+
+    /**
+     * Authentication routes
+     */
+    {
+        method: HTTPMethod.GET,
+        path: "/authenticate",
+    },
+
+    /**
+     * Framework routes
+     */
+    // TODO: Make this route an authenticated route
+    {
+        method: HTTPMethod.GET,
+        path: "/frameworks/:id",
+    },
+    {
+        method: HTTPMethod.POST,
+        path: "/frameworks",
+    },
+    // TODO: Make this route an authenticated route
+    {
+        method: HTTPMethod.PATCH,
+        path: "/frameworks/:id",
+    },
+    // TODO: Make this route an authenticated route
+    {
+        method: HTTPMethod.PATCH,
+        path: "/frameworks/:id/deprecate",
+    },
+    // TODO: Make this route an authenticated route
+    {
+        method: HTTPMethod.DELETE,
+        path: "/frameworks/:id",
+    },
+
+    /**
+     * Guideline routes
+     */
+    // TODO: Make this route an authenticated route
+    {
+        method: HTTPMethod.GET,
+        path: "/frameworks/:id/guidelines",
+    },
+    // TODO: Make this route an authenticated route
+    {
+        method: HTTPMethod.GET,
+        path: "/guidelines/:id",
+    },
+    {
+        method: HTTPMethod.POST,
+        path: "/guidelines",
+    },
+    // TODO: Make this route an authenticated route
+    {
+        method: HTTPMethod.PATCH,
+        path: "/guidelines/:id",
+    },
+    // TODO: Make this route an authenticated route
+    {
+        method: HTTPMethod.PATCH,
+        path: "/guidelines/:id/deprecate",
+    },
+    // TODO: Make this route an authenticated route
+    {
+        method: HTTPMethod.DELETE,
+        path: "/guidelines/:id",
+    },
+
+    /**
+     * Search routes
+     */
+    {
+        method: HTTPMethod.GET,
+        path: "/frameworks",
+    },
+    {
+        method: HTTPMethod.GET,
+        path: "/guidelines",
+    },
+];
