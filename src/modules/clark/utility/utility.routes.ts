902c253dc0b9602f5d6a5652678dcdcb053bf747
Christohper Wagner <cwagne17@students.towson.edu>
2024-01-03 14:54:40 -0500

    add utility routes that have been migrated with TODO notes

--- src/modules/clark/utility/utility.routes.ts
+++ src/modules/clark/utility/utility.routes.ts
@@ -1,3 +1,26 @@
+import { HTTPMethod } from "../../../shared/types/http-method.type";
 import { ProxyRoute } from "../../../shared/types/proxy-route.type";
 
-export const UTILITY_ROUTES: ProxyRoute[] = [];
+export const UTILITY_ROUTES: ProxyRoute[] = [
+    /**
+     * Blog Routes
+     */
+    // TODO: Make this route an authenticated route
+    {
+        method: HTTPMethod.GET,
+        path: "/blogs"
+    },
+    // TODO: Make this route an authenticated route
+    {
+        method: HTTPMethod.POST,
+        path: "/blogs"
+    },
+
+    /**
+     * Client Version Routes
+     */
+    {
+        method: HTTPMethod.GET,
+        path: "/clientversion"
+    }
+];
