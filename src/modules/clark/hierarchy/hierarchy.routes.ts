25de7bc5877372192130749d765469ef97efeac3
Christohper Wagner <cwagne17@students.towson.edu>
2024-01-03 17:08:10 -0500

    add hierarchy routes

--- src/modules/clark/hierarchy/hierarchy.routes.ts
+++ src/modules/clark/hierarchy/hierarchy.routes.ts
@@ -1,3 +1,17 @@
+import { HTTPMethod } from "../../../shared/types/http-method.type";
 import { ProxyRoute } from "../../../shared/types/proxy-route.type";
 
-export const HIERARCHY_ROUTES: ProxyRoute[] = [];
+export const HIERARCHY_ROUTES: ProxyRoute[] = [
+    /**
+     * The following routes have not been declared by
+     * clark-service yet and will have a different target
+     * than clark-service
+     *
+     * The target for the following routes shoud be  hierarchy-service
+     */
+    // TODO: Make this route an authenticated route
+    {
+        method: HTTPMethod.PATCH,
+        path: "/learning-objects/:id/status",
+    },
+];
