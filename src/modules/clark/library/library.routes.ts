a14e0bf925a61fe7b3b4e03fad4fa2f07601bb3d
Christohper Wagner <cwagne17@students.towson.edu>
2024-01-03 14:41:18 -0500

    add library routes

--- src/modules/clark/library/library.routes.ts
+++ src/modules/clark/library/library.routes.ts
@@ -1,3 +1,22 @@
+import { HTTPMethod } from "../../../shared/types/http-method.type";
 import { ProxyRoute } from "../../../shared/types/proxy-route.type";
 
-export const LIBRARY_ROUTES: ProxyRoute[] = [];
+export const LIBRARY_ROUTES: ProxyRoute[] = [
+    // TODO: Make this route an authenticated route
+    {
+        method: HTTPMethod.GET,
+        path: "/users/:username/library"
+    },
+    // TODO: Make this route an authenticated route
+    // ** There is currently a bug in clark-service that doesn't check
+    // for authentication on this route **
+    {
+        method: HTTPMethod.POST,
+        path: "/users/:username/library"
+    },
+    // TODO: Make this route an authenticated route
+    {
+        method: HTTPMethod.DELETE,
+        path: "/users/:username/library/:id"
+    }
+];
