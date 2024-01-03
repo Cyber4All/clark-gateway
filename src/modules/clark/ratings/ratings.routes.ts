07c8b37070dbfb09d6524e1cc98b13e98add9dc0
Christohper Wagner <cwagne17@students.towson.edu>
2024-01-03 14:54:12 -0500

    add ratings routes with TODO for changes that need to be made

--- src/modules/clark/ratings/ratings.routes.ts
+++ src/modules/clark/ratings/ratings.routes.ts
@@ -1,3 +1,73 @@
+import { HTTPMethod } from "../../../shared/types/http-method.type";
 import { ProxyRoute } from "../../../shared/types/proxy-route.type";
 
-export const RATINGS_ROUTES: ProxyRoute[] = [];
+export const RATINGS_ROUTES: ProxyRoute[] = [
+    /**
+     * Rating Flags Routes
+     */
+    // TODO: Make this route an authenticated route
+    {
+        method: HTTPMethod.GET,
+        path: "/ratings/:ratingId/flags"
+    },
+    // TODO: Make this route an authenticated route
+    {
+        method: HTTPMethod.POST,
+        path: "/ratings/:ratingId/flags"
+    },
+    // TODO: Make this route an authenticated route
+    {
+        method: HTTPMethod.DELETE,
+        path: "/ratings/:ratingId/flags/:flagId"
+    },
+
+    /**
+     * Rating Routes
+     */
+    {
+        method: HTTPMethod.GET,
+        path: "/ratings"
+    },
+    {
+        method: HTTPMethod.GET,
+        path: "/learning-objects/:cuid/version/:version/ratings"
+    },
+    // TODO: Make this route an authenticated route
+    {
+        method: HTTPMethod.POST,
+        path: "/learning-objects/:cuid/version/:version/ratings"
+    },
+    // TODO: Make this route an authenticated route
+    {
+        method: HTTPMethod.PATCH,
+        path: "/learning-objects/:cuid/version/:version/ratings/:ratingId"
+    },
+    // TODO: Make this route an authenticated route
+    {
+        method: HTTPMethod.DELETE,
+        path: "/learning-objects/:cuid/version/:version/ratings/:ratingId"
+    },
+
+    /**
+     * Rating Responses Routes
+     */
+    {
+        method: HTTPMethod.GET,
+        path: "/learning-objects/ratings/responses/:responseId"
+    },
+    // TODO: Make this route an authenticated route
+    {
+        method: HTTPMethod.POST,
+        path: "/learning-objects/ratings/:ratingId/responses"
+    },
+    // TODO: Make this route an authenticated route
+    {
+        method: HTTPMethod.PATCH,
+        path: "/learning-objects/ratings/responses/:responseId"
+    },
+    // TODO: Make this route an authenticated route
+    {
+        method: HTTPMethod.DELETE,
+        path: "/learning-objects/ratings/responses/:responseId"
+    }
+];
