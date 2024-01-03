8d11f0a72e0d0d9299c33e0b7bbc05e67e568ef9
Christohper Wagner <cwagne17@students.towson.edu>
2024-01-03 14:16:07 -0500

    add note about featured routes

--- src/modules/clark/featured/featured.routes.ts
+++ src/modules/clark/featured/featured.routes.ts
@@ -2,6 +2,8 @@ import { HTTPMethod } from "../../../shared/types/http-method.type";
 import { ProxyRoute } from "../../../shared/types/proxy-route.type";
 
 export const FEATURED_ROUTES: ProxyRoute[] = [
+    // TODO: Manually test these routes once they are implemented in clark-service
+    // The following routes have been declared in clark-service but not yet implemented
     {
         method: HTTPMethod.GET,
         path: "/featured/learning-objects"
