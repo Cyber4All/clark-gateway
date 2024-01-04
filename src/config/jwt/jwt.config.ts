a2f9696b96e1edc171624f21811e3df216dd7810
Christohper Wagner <cwagne17@students.towson.edu>
2024-01-04 16:22:43 -0500

    add jwt support

---
+++ src/config/jwt/jwt.config.ts
@@ -0,0 +1,45 @@
+import { envConfig } from "../env/env.driver";
+import { AWS_JWT_SECRET, ISSUER } from "../global.env";
+
+/**
+ * The JWT config
+ */
+export class JwtConfig {
+    /**
+     * Gets the Issuer of the Keys
+     *
+     * Production & Staging use the value passed in ENV
+     * Test will use const value
+     * Development will use const if ENV not set
+     *
+     * @returns The Issuer
+     */
+    public static getIssuer(): string {
+        const issuer = envConfig.getValue(ISSUER, false); // won't throw error
+        if ((!issuer && envConfig.isDevelopment()) || envConfig.isTest()) {
+            return "issuer";
+        }
+        return issuer;
+    }
+
+    /**
+     * Returns secret that signs/verifies user JWTs
+     *
+     * Production & Staging use the value passed in ENV
+     * Test will use const value
+     * Development will use const if ENV not set
+     *
+     * @returns The User Token Key
+     */
+    public static getJwtSecret(): string {
+        const key = envConfig.getValue(AWS_JWT_SECRET, false); // won't throw error
+        if (!key && envConfig.isDevelopment()) {
+            return "secret";
+        }
+        if (envConfig.isTest()) {
+            // value must be different for test purposes
+            return "testsecret";
+        }
+        return key;
+    }
+}
