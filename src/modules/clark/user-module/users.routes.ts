4d34550f12507acd2e70edd3b06a12495f4b8158
Christohper Wagner <cwagne17@students.towson.edu>
2024-02-01 12:08:05 -0500

    add user routes

--- src/modules/clark/user-module/users.routes.ts
+++ src/modules/clark/user-module/users.routes.ts
@@ -12,47 +12,18 @@ export const USERS_ROUTES: ProxyRoute[] = [
         method: HTTPMethod.GET,
         path: "/users/:user",
     },
-    {
-        method: HTTPMethod.PATCH,
-        path: "/users",
-        auth: true,
-        target: envConfig.getUri(USER_SERVICE_URI),
-    },
 
     /**
      * The following routes have not been declared by
      * clark-service yet and will have a different target
      * than clark-service
      */
-
-    /**
-     * Mapper routes
-     */
     {
-        method: HTTPMethod.GET,
-        path: "/guidelines/members",
+        method: HTTPMethod.PATCH,
+        path: "/users",
         auth: true,
         target: envConfig.getUri(USER_SERVICE_URI),
     },
-    {
-        method: HTTPMethod.DELETE,
-        path: "/guidelines/members/:memberId",
-        auth: true,
-        target: envConfig.getUri(USER_SERVICE_URI),
-    },
-
-    /**
-     * Personal routes
-     */
-    {
-        method: HTTPMethod.GET,
-        path: "/users/:username/learning-objects",
-        target: envConfig.getUri(USER_SERVICE_URI),
-    },
-
-    /**
-     * Public routes
-     */
     {
         method: HTTPMethod.GET,
         path: "/users/:username/profile",
@@ -61,38 +32,17 @@ export const USERS_ROUTES: ProxyRoute[] = [
     },
     {
         method: HTTPMethod.GET,
-        path: "/users/curators/:collection",
-        target: envConfig.getUri(USER_SERVICE_URI),
-    },
-
-    /**
-     * Recovery routes
-     */
-    // TODO: This will be broken until migrated to clark-service
-    // manually test once migrated
-    {
-        method: HTTPMethod.GET,
         path: "/users/ota-codes",
         target: envConfig.getUri(USER_SERVICE_URI),
     },
-    // TODO: This will be broken until migrated to clark-service
-    // manually test once migrated
     {
         method: HTTPMethod.POST,
         path: "/users/ota-codes",
         target: envConfig.getUri(USER_SERVICE_URI),
     },
-    // TODO: This will be broken until migrated to clark-service
-    // manually test once migrated
     {
         method: HTTPMethod.PATCH,
         path: "/users/ota-codes",
-        target: envConfig.getUri(USER_SERVICE_URI),
-    },
-    {
-        method: HTTPMethod.GET,
-        path: "/validate-captcha",
-        auth: true,
         target: envConfig.getUri(USER_SERVICE_URI),
     },
 ];
