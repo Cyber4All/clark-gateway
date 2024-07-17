7acb94ba16956f04ca784085cd6ddc9a2d8f2a60
Michael Franz <michael.franz@secured.team>
2024-07-17 18:56:31 -0400

    updated auth-module routes; missing validate-captcha

--- src/modules/clark/auth-module/auth.routes.ts
+++ src/modules/clark/auth-module/auth.routes.ts
@@ -2,10 +2,17 @@ import { HTTPMethod } from "../../../shared/types/http-method.type";
 import { ProxyRoute } from "../../../shared/types/proxy-route.type";
 
 export const AUTH_ROUTES: ProxyRoute[] = [
+    /**
+     * Encryption Routes
+     */
     {
         method: HTTPMethod.GET,
         path: "/keys",
     },
+
+    /**
+     * Token Routes
+     */
     {
         method: HTTPMethod.GET,
         path: "/users/tokens",
@@ -16,6 +23,10 @@ export const AUTH_ROUTES: ProxyRoute[] = [
         path: "/users/tokens/refresh",
         auth: true,
     },
+
+    /**
+     * Registration/Login Routes
+     */
     {
         method: HTTPMethod.GET,
         path: "/users/validate",
@@ -30,6 +41,26 @@ export const AUTH_ROUTES: ProxyRoute[] = [
     },
     {
         method: HTTPMethod.GET,
+        path: "/validate-captcha",
+    },
+
+    /**
+     * SSO Routes
+     */
+    {
+        method: HTTPMethod.GET,
+        path: "/google",
+    },
+    {
+        method: HTTPMethod.GET,
+        path: "/google/redirect",
+    },
+
+    /**
+     * Recovery Routes
+     */
+    {
+        method: HTTPMethod.GET,
         path: "/users/ota-code",
     },
     {
@@ -39,13 +70,5 @@ export const AUTH_ROUTES: ProxyRoute[] = [
     {
         method: HTTPMethod.PATCH,
         path: "/users/ota-code",
-    },
-    {
-        method: HTTPMethod.GET,
-        path: "/google",
-    },
-    {
-        method: HTTPMethod.GET,
-        path: "/google/redirect",
-    },
+    }
 ];
