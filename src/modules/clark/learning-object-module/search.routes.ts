1ad4d409f826f32acf15fec9a717deb75c4025f8
Michael Franz <michael.franz@secured.team>
2024-07-17 19:53:25 -0400

    implemented individual route files

---
+++ src/modules/clark/learning-object-module/search.routes.ts
@@ -0,0 +1,13 @@
+import { HTTPMethod } from "../../../shared/types/http-method.type";
+import { ProxyRoute } from "../../../shared/types/proxy-route.type";
+
+export const SEARCH_ROUTES: ProxyRoute[] = [
+    {
+        method: HTTPMethod.GET,
+        path: "/learning-objects",
+    },
+    {
+        method: HTTPMethod.GET,
+        path: "/users/:username/learning-objects",
+    },
+];
