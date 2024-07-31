79e19c0fab74c3dd858e503eb13792eb4847d513
Michael Franz <michael.franz@secured.team>
2024-07-31 13:36:11 -0400

    updated user routes for profile

--- src/modules/clark/collection-module/collections.routes.ts
+++ src/modules/clark/collection-module/collections.routes.ts
@@ -2,12 +2,6 @@ import { HTTPMethod } from "../../../shared/types/http-method.type";
 import { ProxyRoute } from "../../../shared/types/proxy-route.type";
 
 export const COLLECTION_ROUTES: ProxyRoute[] = [
-    // TODO: Move to LO module
-    {
-        method: HTTPMethod.GET,
-        path: "/users/:username/collections",
-    },
-    // ------------------------
     {
         method: HTTPMethod.GET,
         path: "/collections",
