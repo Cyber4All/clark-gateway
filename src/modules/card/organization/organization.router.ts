3c1c54a51fb02aa1933ec004409cb0a18159e3d6
Christopher Wagner <christopher.wagner@secured.team>
2024-10-28 13:33:40 -0400

    rename class

--- src/modules/card/organization/organization.router.ts
+++ src/modules/card/organization/organization.router.ts
@@ -4,7 +4,7 @@ import { CARD_SERVICE_URI } from "../../../config/global.env";
 import { envConfig } from "../../../config/env/env.driver";
 import { ORGANIZATION_ROUTES } from "./organization.routes";
 
-export class OrganizationRouter {
+export class OrganizationRouteHandler{
     public static build(): Router {
         return buildProxyRouter(
             ORGANIZATION_ROUTES,
