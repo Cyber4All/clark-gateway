97c0acc3152b222e6a1cf868c3a77a5bcee943ec
Diego Soto <diegosoto9744@gmail.com>
2024-12-06 12:12:15 -0500

    Removed most Card email routes

--- src/modules/card/email/email.routes.ts
+++ src/modules/card/email/email.routes.ts
@@ -1,25 +1,14 @@
 import { HTTPMethod } from "../../../shared/types/http-method.type";
 import { ProxyRoute } from "../../../shared/types/proxy-route.type";
 
+/**
+ * only one route present here in which would actually be found in the CARD user route handler, 
+ * but we are no longer using CARD user routes apart from this which is for CARD link checking to send emails,
+ * so for better naming convention this is kept as EMAIL_ROUTES. Part of CLARD Unified Users Epic.
+ */
 export const EMAIL_ROUTES: ProxyRoute[] = [
-    {
-        method: HTTPMethod.POST,
-        path: "/users/verify-email",
-    },
-    {
-        method: HTTPMethod.POST,
-        path: "/users/:userId/verify-email-resend",
-    },
     {
         method: HTTPMethod.POST,
         path: "/users/emails/:emailType",
     },
-    {
-        method: HTTPMethod.POST,
-        path: "/users/reset-password",
-    },
-    {
-        method: HTTPMethod.PATCH,
-        path: "/users/reset-password",
-    }
 ];
