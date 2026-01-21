36c81c46b12af809a4a663ec66897745f74900c0
luis <luis.gcodes@gmail.com>
2026-01-21 16:36:20 -0500

    add comment, like every other modules does

--- src/modules/clark/organization/organization.routes.ts
+++ src/modules/clark/organization/organization.routes.ts
@@ -2,6 +2,9 @@ import { HTTPMethod } from "../../../shared/types/http-method.type";
 import { ProxyRoute } from "../../../shared/types/proxy-route.type";
 
 export const ORGANIZATION_ROUTES: ProxyRoute[] = [
+    /**
+     * Search route
+     */
     {
         method: HTTPMethod.GET,
         path: "/organizations",
