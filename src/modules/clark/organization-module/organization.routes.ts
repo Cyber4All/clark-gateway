65d8dc3157f6e139d3503c39f69726f92018c2c6
Christopher Wagner <christopherwagner@Christophers-MacBook-Pro.local>
2026-04-17 13:01:45 -0400

    add migrate route

--- src/modules/clark/organization-module/organization.routes.ts
+++ src/modules/clark/organization-module/organization.routes.ts
@@ -25,5 +25,10 @@ export const ORGANIZATION_ROUTES: ProxyRoute[] = [
         method: HTTPMethod.PATCH,
         auth: true,
         path: "/organizations/:organizationId",
+    },
+    {
+        method: HTTPMethod.POST,
+        auth: true,
+        path: "/organizations/:organizationId/migrate",
     }
 ];
