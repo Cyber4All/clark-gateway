f887e3a0c1dd468e9e0ddae8189032cfc295a5c1
Diego Soto <diegosoto9744@gmail.com>
2026-07-07 11:27:14 -0400

    now using createProxyMiddleware for ClarkMCP

--- src/modules/mcp/clark-mcp.router.ts
+++ src/modules/mcp/clark-mcp.router.ts
@@ -1,11 +1,26 @@
 import { Router } from "express";
-import { buildProxyRouter } from "../../shared/functions/build-proxy-router";
-import { MCP_ROUTES } from "./clark-mcp.routes";
 import { envConfig } from "../../config/env/env.driver";
 import { MCP_SERVICE_URI } from "../../config/global.env";
+import { createProxyMiddleware } from "http-proxy-middleware/dist";
 
 export class ClarkMCPRouteHandler {
     public static build(): Router {
-        return buildProxyRouter(MCP_ROUTES, envConfig.getUri(MCP_SERVICE_URI));
+        const router = Router();
+
+        router.post(
+            "/mcp",
+            createProxyMiddleware({
+                target: envConfig.getUri(MCP_SERVICE_URI),
+                changeOrigin: true,
+                secure:
+                    envConfig.isProduction() || envConfig.isStaging()
+                        ? true
+                        : false,
+                timeout: 120000,
+                proxyTimeout: 120000,
+            }),
+        );
+
+        return router;
     }
 }
